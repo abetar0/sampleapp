021ccec3c02c10b3285222855e81fd8a3106d243
Taro Abe <tarofuku827@gmail.com>
2020-10-03 17:44:43 +0900

    Add JWT authorization feature ref: https://jp.ktor.work/quickstart/guides/api.html#jwt-%E8%AA%8D%E8%A8%BC

--- src/Application.kt
+++ src/Application.kt
@@ -1,12 +1,17 @@
 package net.abetar0
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.jwt
 import io.ktor.features.ContentNegotiation
 import io.ktor.jackson.jackson
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.get
 import io.ktor.routing.post
+import io.ktor.routing.route
 import io.ktor.routing.routing
 import java.util.*
 
@@ -14,31 +19,66 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 data class Snippet(val text: String)
 
-data class PostSnippet(val snippet: PostSnippet.Text) {
+data class PostSnippet(val snippet: Text) {
     data class Text(val text: String)
 }
 val snippets = Collections.synchronizedList(mutableListOf(
     Snippet("Hello"),
     Snippet("World!")
 ))
+
+open class SimpleJWT(val secret: String) {
+    private val algorithm = Algorithm.HMAC256(secret)
+    val verifier = JWT.require(algorithm).build()
+    fun sign(name: String): String = JWT.create().withClaim("name", name).sign(algorithm)
+}
+
+class User(val name: String, val password: String)
+
+val users = Collections.synchronizedMap(
+    listOf(User("test", "test"))
+        .associateBy { it.name }
+        .toMutableMap()
+)
+class LoginRegister(val user: String, val password: String)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    val simpleJwt = SimpleJWT("sy-super-secret-for-jwt")
+    install(Authentication) {
+        jwt {
+            verifier(simpleJwt.verifier)
+            validate {
+                UserIdPrincipal(it.payload.getClaim("name").asString())
+            }
+        }
+    }
     install(ContentNegotiation) {
         jackson {
 
         }
     }
     routing {
-        get("/snippets") {
-            //call.respondText("OK")
-            call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
+        route("/snippets") {
+            get {
+                //call.respondText("OK")
+                call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
+            }
+            authenticate {
+                post {
+                    val post = call.receive<PostSnippet>()
+                    snippets += Snippet(post.snippet.text)
+                    call.respond(mapOf("OK" to true))
+                }
+            }
         }
-        post("snippets") {
-            val post = call.receive<PostSnippet>()
-            snippets += Snippet(post.snippet.text)
-            call.respond(mapOf("OK" to true))
+        post("/login-register") {
+            val post = call.receive<LoginRegister>()
+            val user = users.getOrPut(post.user) { User(post.user, post.password) }
+            if (user.password != post.password) error("Invalid credentials")
+            call.respond(mapOf("token" to simpleJwt.sign(user.name)))
         }
+
     }
 }
 
